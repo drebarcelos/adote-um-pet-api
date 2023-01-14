ee8fdd5623789dce6370c1b180da5ae37d8edd23
Andressa Barcelos <andressa.barcelos26@gmail.com>
2023-01-14 20:39:34 -0300

    build: estrutura inicial do projeto

---
+++ src/main/java/br/com/adote/um/pet/AdoteUmPetApplication.java
@@ -0,0 +1,14 @@
+package br.com.adote.um.pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoteUmPetApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AdoteUmPetApplication.class, args);
+	}
+
+}
