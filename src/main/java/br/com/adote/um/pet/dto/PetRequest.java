216b6785fdae4f6edd3b4498258c45deaa554752
Andressa Barcelos <andressa.barcelos26@gmail.com>
2023-01-15 21:55:18 -0300

    feat: adiciona PetRequest com atributos necessários para o cadastro de Pet

---
+++ src/main/java/br/com/adote/um/pet/dto/PetRequest.java
@@ -0,0 +1,16 @@
+package br.com.adote.um.pet.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PetRequest {
+    private String name;
+    private String specie;
+    private String gender;
+    private String size;
+    private Integer age;
+    private String coatLength;
+    private String behavior;
+}
