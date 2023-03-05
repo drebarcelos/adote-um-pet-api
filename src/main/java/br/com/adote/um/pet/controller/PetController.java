ae2a56f631007bc289a3232216b78004ad0c4653
Andressa Barcelos <andressa.barcelos26@gmail.com>
2023-03-04 21:11:48 -0300

    feat: adiciona endpoint get no controller de pet

--- src/main/java/br/com/adote/um/pet/controller/PetController.java
+++ src/main/java/br/com/adote/um/pet/controller/PetController.java
@@ -1,13 +1,17 @@
 package br.com.adote.um.pet.controller;
 
 import br.com.adote.um.pet.dto.PetRequest;
+import br.com.adote.um.pet.entity.Pet;
 import br.com.adote.um.pet.service.PetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +22,10 @@ public class PetController {
     @PostMapping(path = "/cadastro/pet")
     public ResponseEntity<Object> petRegister(@RequestBody PetRequest petRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(petService.savePet(petRequest));
+    }
+
+    @GetMapping("/pets")
+    public List<Pet> getRegisters(){
+        return petService.getAllPets();
     }
 }
