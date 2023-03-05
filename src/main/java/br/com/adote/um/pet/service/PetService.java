e29baf2bfe4102cd0ac3eeab342b684718c770f0
Andressa Barcelos <andressa.barcelos26@gmail.com>
2023-03-04 23:10:28 -0300

    feat: adiciona metodo getPet na service para buscar um pet pelo id

--- src/main/java/br/com/adote/um/pet/service/PetService.java
+++ src/main/java/br/com/adote/um/pet/service/PetService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +30,9 @@ public class PetService {
 
     public List<Pet> getAllPets(){
         return petReporitory.findAll();
+    }
+
+    public Optional<Pet> getPet(Long id){
+        return petReporitory.findById(id);
     }
 }
